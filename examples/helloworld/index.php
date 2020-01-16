3351d9a2d9145b7be5c1813886bcb15afb9bd812
villepreux <villepreux@users.noreply.github.com>
2020-01-16 16:43:25 +0100

    Update index.php

--- examples/helloworld/index.php
+++ examples/helloworld/index.php
@@ -1,12 +1,12 @@
 <?php
 
-include("dom.php"); 
+include("dom.php"); // include de DOM.php framework
 
-doc_header();
+doc_header(); // Set document header (utf8 html by default)
 
-doc_output(html(
-  head().
-  body(
+doc_output(html( // Output the document
+  head(). // HTML standard header, with educated defaults
+  body( // Then some regular content
     h1("Hello World!").
     p("This is a Hello World example.").
     "")
