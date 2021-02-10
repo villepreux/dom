a5e906458d840ef4a9e4b286453dd8a8cd45803c
villepreux <villepreux@users.noreply.github.com>
2021-02-10 09:56:00 +0000

    Fixes by misspell-fixer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6b5710fa5630f21d1e49a5e47ea2a303cc8aa70f',
+    'name' => 'villepreux/dom',
+  ),
+  'versions' => 
+  array (
+    'villepreux/dom' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b5710fa5630f21d1e49a5e47ea2a303cc8aa70f',
+    ),
+  ),
+);
