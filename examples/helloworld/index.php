7b4ec1666b130bafaf73b1788101f65ba9e448fc
villepreux <villepreux@gmail.com>
2024-01-09 11:51:57 +0100

    Force rebuild

--- examples/helloworld/index.php
+++ examples/helloworld/index.php
@@ -4,6 +4,6 @@ use function dom\{init,output,head,body,main,h1,p,pre,content};
 init();
 output(head().body(main(
     h1("Hello World!").        
-    p("This is a Hello World example").
+    p("This is a 'Hello World' example").
     pre(htmlentities(content("index.php")))
     )));
