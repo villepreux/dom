9528798c9b23bc0d05b59d2e4b2430d2d447fc84
villepreux <villepreux@gmail.com>
2024-01-09 12:53:32 +0100

    Fix some internal links

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
 
 require_once dirname(__FILE__)."/dom_html.php";
-
 use function dom\{set,get,is_localhost,init,output,markdown,path,include_file,head,body,main,style};
-
-set("generate", false);
-
-    // TODO ------------------------------------------------------------------------>
-    // TODO : FIX CANONICAL PATH HACK
-
-    if (!!get("static"))
-    {
-        set("canonical", rtrim(str_replace("www.villepreux.net",    "villapirorum.netlify.app", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("villepreux.net",        "villapirorum.netlify.app", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("http://localhost",      "https://",                 get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("http://127.0.0.1",      "https://",                 get("canonical")), "/"));
-    }
-
-    if (is_localhost())
-    {
-        set("canonical", rtrim(str_replace("https://localhost", "http://localhost", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("https://127.0.0.1", "http://127.0.0.1", get("canonical")), "/"));
-    }
-
-    set("canonical", str_replace("///", "//", get("canonical")));
-
-    // TODO
-    // TODO ------------------------------------------------------------------------>
 
 init();
 
