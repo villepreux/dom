52a026e69b9a8f03eb0d29dfa1ab06314f9f981d
villepreux <villepreux@gmail.com>
2024-01-11 08:07:54 +0100

    Cleanup

--- index.php
+++ index.php
@@ -1,42 +1,25 @@
 <?php
 
-require_once dirname(__FILE__)."/dom_html.php";
-
-use function dom\{set,get,is_localhost,init,output,markdown,path,include_file,head,body,main,style};
-
-set("generate", false);
-
-    // TODO ------------------------------------------------------------------------>
-    // TODO : FIX CANONICAL PATH HACK
-
-    if (!!get("static"))
-    {
-        set("canonical", rtrim(str_replace("www.villepreux.net",    "villapirorum.netlify.app", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("villepreux.net",        "villapirorum.netlify.app", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("http://localhost",      "https://",                 get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("http://127.0.0.1",      "https://",                 get("canonical")), "/"));
-    }
-
-    if (is_localhost())
-    {
-        set("canonical", rtrim(str_replace("https://localhost", "http://localhost", get("canonical")), "/"));
-        set("canonical", rtrim(str_replace("https://127.0.0.1", "http://127.0.0.1", get("canonical")), "/"));
-    }
-
-    set("canonical", str_replace("///", "//", get("canonical")));
-
-    // TODO
-    // TODO ------------------------------------------------------------------------>
+require_once(__DIR__."/dom_html.php");
+use function dom\{set,get,is_localhost,init,output,HSTART,HERE,HSTOP,markdown,path,include_file,head,body,main,style};
 
 init();
 
 output(
     head().
     body(
-        style("img[alt=\"Build\"] { width: 105px; height: 20px; }").
+        style((function(){HSTART()?><style><?=HERE()?>         
+            img[alt="Build"] { 
+                width:  105px; 
+                height:  20px; 
+            }
+            <?=HERE()?></style><?php return HSTOP();})()).
         main(
             markdown(
-                str_replace("https://github.com/villepreux/dom/tree/master/examples", "examples", include_file(path("README.md")))
+                str_replace("20XX", date("Y"), 
+                str_replace("https://github.com/villepreux/dom/tree/master/", "", 
+                    include_file(path("README.md"))
+                    ))
                 )
             )
         )
