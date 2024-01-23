69e5d90d1a56a2ebc010b214a557cc76cecc577a
villepreux <villepreux@gmail.com>
2024-01-23 12:20:27 +0100

    DOM examples

--- examples/complete/index.php
+++ examples/complete/index.php
@@ -65,51 +65,52 @@ output(
 
             dom.on_ready(function() {
 
+                /* Colors contrast utilities */
+
                 function getsRGB(c) {
             
                     c = parseInt(c, 16) / 255;
                     c = (c <= 0.03928) ? c / 12.92 : Math.pow(((c + 0.055) / 1.055), 2.4);
                     
                     return c;
-                    }
+                }
             
                 function hex(x) {
+
                     return ("0" + parseInt(x).toString(16)).slice(-2);
-                    }
+                }
             
                 function rgb_to_hex(c) {
-            
+        
                     if (c.search("rgba") >= 0) { c = c.match(/^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)$/); return "#" + hex(c[1]) + hex(c[2]) + hex(c[3]); }
                     if (c.search("rgb")  >= 0) { c = c.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);           return "#" + hex(c[1]) + hex(c[2]) + hex(c[3]); }
             
                     return c;
-                    }
-            
+                }
+        
                 function get_luminosity(c) {
             
                     c = rgb_to_hex(c);
             
                     return (0.2126 * getsRGB(c.substr(1, 2)) 
-                        + 0.7152 * getsRGB(c.substr(3, 2)) 
-                        + 0.0722 * getsRGB(c.substr(  -2)) );
-            
-                    }
+                          + 0.7152 * getsRGB(c.substr(3, 2)) 
+                          + 0.0722 * getsRGB(c.substr(  -2)) );            
+                }
+
+                /* Color scheme switch */
             
                 function set_theme(theme) { 
                 
                     document.documentElement.setAttribute("data-colorscheme", theme);
-                
-                    }
-            
+                }
+        
                 document.querySelectorAll(".darkandlight").forEach(function(e) { e.addEventListener("click", function() {
             
                     var background_color = window.getComputedStyle(document.querySelector("body")).getPropertyValue("background-color");
-            
                     var is_dark = get_luminosity(background_color) < 0.5;
-            
                     set_theme(is_dark ? "light" : "dark");
-            
-                    }); });
+        
+                }); });
 
             });
 
@@ -147,30 +148,23 @@ output(
                                 var scrollMargin = 16 + parseInt(window.getComputedStyle(x).getPropertyValue("scroll-margin-top").replace("px",""));
                                 
                                 if (window.scrollY >= (x.getBoundingClientRect().top + window.pageYOffset - scrollMargin)
-                                ||  window.scrollY >= (document.body.offsetHeight    - window.innerHeight - scrollMargin) ) 
-                                
-                                    current_link = e;
-
-                                }
+                                ||  window.scrollY >= (document.body.offsetHeight    - window.innerHeight - scrollMargin) ) current_link = e;
                             }
-                        });
+                        }
+                    });
                 
                     if (current_link != null) {
 
-                        document.querySelectorAll(".toolbar-row-nav .toolbar-cell-right a").forEach(function(e) {
-                            e.classList.remove("current");
-                            });
-                        
+                        document.querySelectorAll(".toolbar-row-nav .toolbar-cell-right a").forEach(function(e) { e.classList.remove("current"); });                    
                         current_link.classList.add("current");
-                        
-                        }
                     }
+                }
 
                 update_current_link_from_scroll();				
-                window.addEventListener("scroll", update_current_link_from_scroll);
-            
+                window.addEventListener("scroll", update_current_link_from_scroll);            
             });
     
             <?= HERE("raw_js") ?></script><?php return HSTOP(); } )())).
+
     rss(). // I'm also interested in having a RSS feed...
     jsonfeed()); // ... and json-content from my content
